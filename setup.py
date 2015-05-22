6868af7c87881c5007fa383b0c572f5b56d00207
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2015-05-22 12:44:40 +0100

    First commit.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from distutils.core import setup
+
+import urllib
+import setuptools.command.install
+
+
+setup(name='python_mozaggregator',
+      version='0.1',
+      author='Roberto Agostino Vitillo',
+      author_email='rvitillo@mozilla.com',
+      description='Telemetry aggregation job',
+      url='https://github.com/vitillo/python_mozaggregator',
+      packages=['aggregator'],
+      package_dir={'aggregator': 'aggregator'},
+      install_requires=['python_moztelemtry'])
+
