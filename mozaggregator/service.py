c296e15fa692c4a83e072429df3b38386e653d02
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2015-06-03 15:24:37 +0000

    Refactor query execution and use a new connection for each request

--- mozaggregator/service.py
+++ mozaggregator/service.py
@@ -6,41 +6,46 @@ from db import create_connection
 
 app = Flask(__name__)
 
+
+def execute_query(query, params=tuple()):
+    db = create_connection(host=host)
+    cursor = db.cursor()
+    cursor.execute(query, params)
+    return cursor.fetchall()
+
+
 @app.route('/channel/')
 def get_channels():
     try:
-        cursor = db.cursor()
-        cursor.execute("""select * from list_channels()""")
-        channels = cursor.fetchall()
+        channels = execute_query("select * from list_channels()")
         return json.dumps([channel[0] for channel in channels])
     except:
         abort(404)
 
-@app.route('/channel/<channel>/buildid')
+
+@app.route('/channel/<channel>/buildid/')
 def get_buildids(channel):
     try:
-        cursor = db.cursor()
-        cursor.execute("select * from list_buildids(%s)", (channel, ))
-        result = cursor.fetchall()
+        result = execute_query("select * from list_buildids(%s)", (channel, ))
         pretty_result = map(lambda r: {"version": r[0], "buildid": r[1]}, result)
         return json.dumps(pretty_result)
     except:
         abort(404)
 
+
 @app.route('/channel/<channel>/buildid/<version>_<buildid>', methods=["GET"])
 def get_buildid(channel, version, buildid):
     try:
         dimensions = request.args.get("dimensions", "{}")
-        cursor = db.cursor()
-        cursor.execute("select * from get_buildid_metric(%s, %s, %s, %s)", (channel, version, buildid, dimensions))
-        result = cursor.fetchall()
+        result = execute_query("select * from get_buildid_metric(%s, %s, %s, %s)", (channel, version, buildid, dimensions))
         pretty_result = map(lambda r: {"label": r[0], "histogram": r[1]}, result)
         return json.dumps(pretty_result)
     except:
         abort(404)
 
+
 if __name__ == "__main__":
-    global db
+    global host
 
     parser = argparse.ArgumentParser(description="Aggregation REST service", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
@@ -49,6 +54,6 @@ if __name__ == "__main__":
 
     parser.set_defaults(debug=False)
     args = parser.parse_args()
+    host = args.host
 
-    db = create_connection(host=args.host)
-    app.run(debug=args.debug)
+    app.run("0.0.0.0", debug=args.debug)
