b7474972e0b2653d8ace4c8f14e75bde20d03c22
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2015-07-21 14:30:19 +0100

    Fix typo.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='python_mozaggregator',
       version='0.2.1.8',
