3eaf42f29737b811047d3e2eeec7f45ea67ea6a9
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2016-05-11 15:12:36 +0000

    Bump version up.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup
 
 setup(name='python_mozaggregator',
-    version='0.2.5.0',
+    version='0.2.5.1',
     author='Roberto Agostino Vitillo',
     author_email='rvitillo@mozilla.com',
     description='Telemetry aggregation job',
