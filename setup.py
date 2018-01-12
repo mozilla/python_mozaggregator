71f9ee703f5ab4c1804e2410e478c67a77d69631
Frank Bertsch <fbertsch@mozilla.com>
2018-01-12 12:30:50 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup
 
 setup(name='python_mozaggregator',
-    version='0.2.6.10',
+    version='0.2.6.11',
     author='Roberto Agostino Vitillo',
     author_email='rvitillo@mozilla.com',
     description='Telemetry aggregation job',
