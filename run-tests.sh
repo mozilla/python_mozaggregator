afa6f288b5f897e94d7bf50eff1107e4e26b269d
Rob Hudson <robhudson@mozilla.com>
2018-03-08 15:38:27 -0800

    Update Docker config to simplify test runner

--- run-tests.sh
+++ run-tests.sh
@@ -6,25 +6,6 @@ if [ -z "$SPARK_HOME" ]; then
     exit 1
 fi
 
-if [ -z "$POSTGRES_HOST" ]; then
-    echo 'You need to set $POSTGRES_HOST to run these tests.' >&2
-    exit 1
-fi
-
-if [ -z "$POSTGRES_USER" ]; then
-    echo 'You need to set $POSTGRES_USER to run these tests.' >&2
-    exit 1
-fi
-
-# See https://github.com/travis-ci/travis-ci/issues/7940
-export BOTO_CONFIG=/dev/null
-export DB_TEST_URL="dbname=postgres user=${POSTGRES_USER} host=${POSTGRES_HOST}"
-
-# Allow mozaggregator to be imported.
-export PYTHONPATH=$PYTHONPATH:.
-
-python ./mozaggregator/service.py -d &
-
 python "$(which nosetests)" --with-coverage --cover-package=mozaggregator ./tests/*.py || exit 1
 
 # Show a text coverage report after a test run.
