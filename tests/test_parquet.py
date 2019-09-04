3891d28bc06e1666fe418d59d7a703e833ac6716
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-09-04 16:21:25 -0700

    Add tests for cli runners

--- tests/test_parquet.py
+++ tests/test_parquet.py
@@ -3,9 +3,11 @@ import unittest
 
 import pyspark
 import pytest
+from click.testing import CliRunner
 
 import dataset as d
 from mozaggregator.parquet import _aggregate_metrics
+from mozaggregator.cli import run_parquet
 
 
 class testParquetAggregation(unittest.TestCase):
@@ -32,3 +34,50 @@ class testParquetAggregation(unittest.TestCase):
         self.assertEqual(
             len(pings) / d.NUM_PINGS_PER_DIMENSIONS / num_build_ids,
             len(self.submission_date_aggs))
+
+
+@pytest.fixture()
+def raw_pings():
+    return list(d.generate_pings())
+
+
+def test_parquet_aggregation_cli(tmp_path, monkeypatch, spark, raw_pings):
+    output = str(tmp_path / "output")
+
+    class Dataset:
+        @staticmethod
+        def from_source(*args, **kwargs):
+            return Dataset()
+
+        def where(self, *args, **kwargs):
+            self.is_fennec = kwargs.get("docType") == "saved_session"
+            return self
+
+        def records(self, *args, **kwargs):
+            if self.is_fennec:
+                return spark.sparkContext.emptyRDD()
+            else:
+                return spark.sparkContext.parallelize(raw_pings)
+
+    monkeypatch.setattr("mozaggregator.parquet.Dataset", Dataset)
+
+    result = CliRunner().invoke(
+        run_parquet,
+        [
+            "--date",
+            "20190901",
+            "--channels",
+            "nightly,beta",
+            "--output",
+            output,
+            "--num-partitions",
+            10,
+        ],
+        catch_exceptions=False,
+    )
+
+    assert result.exit_code == 0
+
+    df = spark.read.parquet(output)
+    # 31104 is the empirical count from the generated pings
+    assert df.count() > len(raw_pings)
