7d4843963bd01882862e32e9ac2a04a7f52bb391
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-10-29 15:54:01 -0700

    Add fixture and utils for BigQuery tests

---
+++ tests/utils.py
@@ -0,0 +1,67 @@
+import base64
+import json
+import os
+import zlib
+from datetime import datetime
+
+
+def format_payload_bytes_decoded(ping):
+    # fields are created in tests/dataset.py
+    return {
+        "normalized_app_name": ping["application"]["name"],
+        "normalized_channel": ping["application"]["channel"],
+        "normalized_os": ping["environment"]["system"]["os"]["name"],
+        "metadata": {
+            "uri": {
+                "app_version": ping["application"]["version"],
+                "app_build_id": ping["application"]["buildId"],
+                "app_name": ping["application"]["name"],
+            }
+        },
+        "sample_id": ping["meta"]["sampleId"],
+        "submission_timestamp": datetime.strptime(
+            ping["meta"]["submissionDate"], "%Y%m%d"
+        ).strftime("%Y-%m-%d %H:%M:%S"),
+        "payload": base64.b64encode(zlib.compress(json.dumps(ping))),
+    }
+
+
+def format_payload_bytes_decoded_mobile(ping):
+    """Format the mobile payload, this requires less meta information than the
+    normal dataset because there is little to no filtering being done in the job.
+
+    Fields are created in tests/mobile_dataset.py.
+    """
+    return {
+        "submission_timestamp": datetime.strptime(
+            ping["meta"]["submissionDate"], "%Y%m%d"
+        ).strftime("%Y-%m-%d %H:%M:%S"),
+        "normalized_channel": ping["meta"]["normalizedChannel"],
+        "metadata": {
+            "uri": {
+                "app_version": str(ping["meta"]["appVersion"]),
+                "app_build_id": ping["meta"]["appBuildId"],
+                "app_name": ping["meta"]["appName"],
+            }
+        },
+        "payload": base64.b64encode(zlib.compress(json.dumps(ping))),
+    }
+
+
+def runif_bigquery_testing_enabled(func):
+    """A decorator that will skip the test if the current environment is not set up for running tests.
+
+        @runif_bigquery_testing_enabled
+        def test_my_function_that_uses_bigquery_spark_connector(table_fixture):
+            ...
+    """
+    # importing this at module scope will break test discoverability
+    import pytest
+
+    bigquery_testing_enabled = os.environ.get(
+        "GOOGLE_APPLICATION_CREDENTIALS"
+    ) and os.environ.get("PROJECT_ID")
+    return pytest.mark.skipif(
+        not bigquery_testing_enabled,
+        reason="requires valid gcp credentials and project id",
+    )(func)
